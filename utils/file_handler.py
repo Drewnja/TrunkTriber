f99992ac9d7406d25556f0c625d7556a7cb7593f
Drewnja <admin@p0chemy.ru>
2024-08-22 16:09:01 +0300

    new gitignore

--- utils/file_handler.py
+++ utils/file_handler.py
@@ -1,13 +1,12 @@
 import os
 import re
 from datetime import datetime
-from threading import Thread
+from threading import Thread, Event
 from watchdog.events import FileSystemEventHandler
 from utils import display
 from utils.display import progress_indicator, stop_progress
 from colorama import Fore, Style
 from models.whisper_model import transcribe_audio
-from threading import Event
 
 # Create a global event to control the progress indicator
 stop_progress_event = Event()
@@ -34,12 +33,12 @@ class AudioHandler(FileSystemEventHandler):
 
         # Extract FROM and TO numbers using regex
         from_number, to_number = self.extract_from_to_numbers(filename)
-        if from_number and to_number:
+        
+        if from_number or to_number:
             self.transcribe_and_print_result(file_path, formatted_creation_datetime, from_number, to_number)
         else:
             print(f"{Fore.YELLOW} [⚠️] SDRTrunk filename format not found{Style.RESET_ALL}")
             self.transcribe_and_print_result(file_path, formatted_creation_datetime)
-            
 
     def extract_from_to_numbers(self, filename):
         match = re.search(r'TO_(\d+)_FROM_(\d+)', filename)
@@ -49,7 +48,7 @@ class AudioHandler(FileSystemEventHandler):
             return from_number, to_number
         return None, None
 
-    def transcribe_and_print_result(self, file_path, formatted_creation_datetime, from_number = '', to_number = ''):
+    def transcribe_and_print_result(self, file_path, formatted_creation_datetime, from_number=None, to_number=None):
         # Start the progress indicator
         global stop_progress
         stop_progress_event.clear()
@@ -62,12 +61,16 @@ class AudioHandler(FileSystemEventHandler):
         # Stop the progress indicator
         stop_progress_event.set()
         progress_thread.join()
-        
 
-        # Format and print the output with hyphen
-        print(f"{Fore.GREEN + Style.BRIGHT}{formatted_creation_datetime}{Style.RESET_ALL} "
-              f"{Fore.BLUE}FROM {from_number}{Style.RESET_ALL} "
-              f"{Fore.RED}TO {to_number}{Style.RESET_ALL} - "
-              f"{Fore.WHITE + Style.BRIGHT}{result}{Style.RESET_ALL}")
+        # Build the output message
+        from_text = f"{Fore.BLUE}FROM {from_number}{Style.RESET_ALL}" if from_number else ""
+        to_text = f"{Fore.RED}TO {to_number}{Style.RESET_ALL}" if to_number else ""
+
+        if from_text or to_text:
+            print(f"{Fore.GREEN + Style.BRIGHT}{formatted_creation_datetime}{Style.RESET_ALL} "
+                  f"{from_text} {to_text} - "
+                  f"{Fore.WHITE + Style.BRIGHT}{result}{Style.RESET_ALL}")
+        else:
+            print(f"{Fore.YELLOW} [⚠️] SDRTrunk filename format not found. Transcription: {Fore.WHITE + Style.BRIGHT}{result}{Style.RESET_ALL}")
 
         display.print_waiting_message()
